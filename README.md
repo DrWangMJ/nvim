99f2c9e24c1c7278a6b2a00c320d9c163f65a7a5
Mingjun Wang <none.wmj@live.com>
2022-10-13 18:16:27 +0800

    add null-ls support

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # My configuration for Neovim
 
 ## Requests
+- Nodejs > 12.0
+- fd 
+- ripgrep
+- unzip
 
 ## Key mappings
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,6 +13,11 @@ return require('packer').startup(function(use)
   use 'williamboman/mason-lspconfig.nvim'
   use 'neovim/nvim-lspconfig'
 
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-file-browser.nvim'
+  use 'kyazdani42/nvim-web-devicons'
+
   use 'hrsh7th/cmp-nvim-lsp'
   use 'hrsh7th/cmp-buffer'
   use 'hrsh7th/cmp-path'
@@ -23,16 +28,14 @@ return require('packer').startup(function(use)
   use 'saadparwaiz1/cmp_luasnip'
   use 'onsails/lspkind-nvim'
 
+  use 'jose-elias-alvarez/null-ls.nvim'
+  use 'jayp0521/mason-null-ls.nvim'
+
   use {
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
   }
 
-  use 'nvim-lua/plenary.nvim'
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
   use 'numToStr/Comment.nvim'
