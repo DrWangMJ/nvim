33fe8b817775bb5ae1cfbd01a6f9781274000acd
Mingjun Wang <none.wmj@live.com>
2022-10-18 22:47:56 +0800

    auto format after write

--- lua/p-null.lua
+++ lua/p-null.lua
@@ -1,23 +1,43 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.stylua,
-  },
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.black,
+		null_ls.builtins.formatting.isort,
+		null_ls.builtins.formatting.remark,
+	},
+
+	on_attach = function(client)
+		-- NOTE: 如果想要禁止某种语言在save时format，可以添加判定
+		-- if client.name == "xxx" then
+		--
+		-- end
+		-- auto format when save file
+    print(client.server_capabilities.documentFormattingProvider)
+		if client.server_capabilities.documentFormattingProvider then
+			vim.cmd([[
+            augroup LspFormatting
+            autocmd! * <buffer>
+            autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
+            augroup END
+            ]])
+		end
+	end,
 })
+
 
 local mason_null_ls = require("mason-null-ls")
-
 mason_null_ls.setup({
-  ensure_installed = { "stylua", },
-  automatic_installation = false,
+	ensure_installed = { "stylua", "black", "remark" },
+	automatic_installation = false,
 })
 
-mason_null_ls.setup_handlers {
-    function(source_name)
-      -- all sources with no handler get passed here
-    end,
-    stylua = function()
-      null_ls.register(null_ls.builtins.formatting.stylua)
-    end,
-}
+-- mason_null_ls.setup_handlers {
+--     function(source_name)
+--       -- all sources with no handler get passed here
+--     end,
+--     stylua = function()
+--       null_ls.register(null_ls.builtins.formatting.stylua)
+--     end,
+-- }
