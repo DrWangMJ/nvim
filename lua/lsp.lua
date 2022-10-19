da991f09035e63a82c2449085036cb05aaee5c3c
DrWangMJ <none.wmj@live.com>
2022-10-19 15:53:38 +0800

    V0.1

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,31 +1,32 @@
 -- mason lspconfig
 local _, mason_lspconfig = pcall(require, "mason-lspconfig")
 mason_lspconfig.setup({
-  ensure_installed = { "sumneko_lua", "pyright", "remark_ls" }, 
-  automatic_installation = false,
+	ensure_installed = { "sumneko_lua", "pyright", "remark_ls" },
+	automatic_installation = false,
 })
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
 end
 
 -- require('lspconfig')['pyright'].setup{
@@ -35,12 +36,12 @@ end
 
 local _, lspconfig = pcall(require, "lspconfig")
 
-mason_lspconfig.setup_handlers {
--- This is a default handler that will be called for each installed server (also for new servers that are installed during a session)
-  function (server_name)
-    lspconfig[server_name].setup {
-      on_attach = on_attach,
-      flags = lsp_flags,
-    }
-  end,
-}
+mason_lspconfig.setup_handlers({
+	-- This is a default handler that will be called for each installed server (also for new servers that are installed during a session)
+	function(server_name)
+		lspconfig[server_name].setup({
+			on_attach = on_attach,
+			flags = lsp_flags,
+		})
+	end,
+})
