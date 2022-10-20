027eb7874aa06f91d0dbe14642b5a15aebbe9718
DrWangMJ <none.wmj@live.com>
2022-10-20 11:08:41 +0800

    add which-keys

--- lua/p-telescope.lua
+++ lua/p-telescope.lua
@@ -1,75 +1,77 @@
 local actions = require("telescope.actions")
 
 function telescope_buffer_dir()
-  return vim.fn.expand('%:p:h')
+	return vim.fn.expand("%:p:h")
 end
 
-local fb_actions = require 'telescope'.extensions.file_browser.actions
+local fb_actions = require("telescope").extensions.file_browser.actions
 
-require('telescope').setup{
-  defaults = {
-    -- Default configuration for telescope goes here:
-    -- config_key = value,
-    mappings = {
-      i = {
-        -- map actions.which_key to <C-h> (default: <C-/>)
-        -- actions.which_key shows the mappings for your picker,
-        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
-        ["esc"] = actions.close,
-        ['<C-j>'] = actions.move_selection_next,
-        ['<C-k>'] = actions.move_selection_previous,
-        ['<C-n>'] = actions.cycle_history_next,
-        ['<C-p>'] = actions.cycle_history_prev
-      }
-    }
-  },
-  layout_config = {
-    horizontal = {
-      preview_cutoff = 150,
-      preview_width = 0.7,
-    }
-  },
-  pickers = {
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-  },
-  extensions = {
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-    file_browser = {
-      theme = 'dropdown',
-      hijack_netrw = true,
-      mappings = {
-        ['i'] = {
-          ['<C-w>'] = function() vim.cmd('normal vbd') end,
-          ['<C-h>'] = fb_actions.goto_parent_dir,
-        },
-        ['n'] = {
-          ['h'] = fb_actions.goto_parent_dir,
-          ['/'] = function()
-            vim.cmd('startinsert')
-          end
-        }
-      }
-    }
-  }
-}
+require("telescope").setup({
+	defaults = {
+		-- Default configuration for telescope goes here:
+		-- config_key = value,
+		mappings = {
+			i = {
+				-- map actions.which_key to <C-h> (default: <C-/>)
+				-- actions.which_key shows the mappings for your picker,
+				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
+				["esc"] = actions.close,
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
+			},
+		},
+	},
+	layout_config = {
+		horizontal = {
+			preview_cutoff = 150,
+			preview_width = 0.7,
+		},
+	},
+	pickers = {
+		-- Default configuration for builtin pickers goes here:
+		-- picker_name = {
+		--   picker_config_key = value,
+		--   ...
+		-- }
+		-- Now the picker_config_key will be applied every time you call this
+		-- builtin picker
+	},
+	extensions = {
+		-- Your extension configuration goes here:
+		-- extension_name = {
+		--   extension_config_key = value,
+		-- }
+		-- please take a look at the readme of the extension you want to configure
+		file_browser = {
+			theme = "dropdown",
+			hijack_netrw = true,
+			mappings = {
+				["i"] = {
+					["<C-w>"] = function()
+						vim.cmd("normal vbd")
+					end,
+					["<C-h>"] = fb_actions.goto_parent_dir,
+				},
+				["n"] = {
+					["h"] = fb_actions.goto_parent_dir,
+					["/"] = function()
+						vim.cmd("startinsert")
+					end,
+				},
+			},
+		},
+	},
+})
 
-require('telescope').load_extension('file_browser')
+require("telescope").load_extension("file_browser")
 
---local opts = {noremap=true, silent = true}
+local opts = { noremap = true, silent = true }
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', 'ff', builtin.find_files, {})
-vim.keymap.set('n', 'fg', builtin.live_grep, {})
-vim.keymap.set('n', 'fb', builtin.buffers, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "ff", builtin.find_files, {})
+vim.keymap.set("n", "fg", builtin.live_grep, {})
+vim.keymap.set("n", "fb", builtin.buffers, {})
+vim.keymap.set("n", "fr", builtin.oldfiles, {})
 --vim.keymap.set('n', 'fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fb', '<cmd>lua require("telescope").extensions.file_browser.file_browser({path="%:p:h", cwd=telescope_buffer_dir(), respect_git_ignore=false, hidden=true})<CR>', opts)
