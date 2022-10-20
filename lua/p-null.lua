6dffdfeba7b9995370be0fb43d9dda05f2786402
Mingjun Wang <none.wmj@live.com>
2022-10-20 11:56:10 +0800

    modified format settings

--- lua/p-null.lua
+++ lua/p-null.lua
@@ -18,7 +18,7 @@ null_ls.setup({
 			vim.cmd([[
             augroup LspFormatting
             autocmd! * <buffer>
-            autocmd BufWritePre <buffer> lua vim.lsp.buf.format()
+            autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()
             augroup END
             ]])
 		end
