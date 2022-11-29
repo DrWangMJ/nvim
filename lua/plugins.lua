39a9d7ff8aa80e2921ae18ba507753cbe3df4b0d
Mingjun Wang <none.wmj@live.com>
2022-11-29 10:00:20 +0800

    add lastplace

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,70 +4,71 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
-	use("sainnhe/everforest")
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
+  use("sainnhe/everforest")
 
-	-- lsp
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("neovim/nvim-lspconfig")
+  -- lsp
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
+  use("neovim/nvim-lspconfig")
 
-	-- telescope
-	use("nvim-lua/plenary.nvim")
-	use("nvim-telescope/telescope.nvim")
-	use("nvim-telescope/telescope-file-browser.nvim")
-	use("kyazdani42/nvim-web-devicons")
+  -- telescope
+  use("nvim-lua/plenary.nvim")
+  use("nvim-telescope/telescope.nvim")
+  use("nvim-telescope/telescope-file-browser.nvim")
+  use("kyazdani42/nvim-web-devicons")
 
-	-- completion
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/cmp-nvim-lua")
-	use("hrsh7th/nvim-cmp")
-	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip")
-	use("onsails/lspkind-nvim")
+  -- completion
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("hrsh7th/cmp-cmdline")
+  use("hrsh7th/cmp-nvim-lua")
+  use("hrsh7th/nvim-cmp")
+  use("L3MON4D3/LuaSnip")
+  use("saadparwaiz1/cmp_luasnip")
+  use("onsails/lspkind-nvim")
 
-	-- formation
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("jayp0521/mason-null-ls.nvim")
+  -- formation
+  use("jose-elias-alvarez/null-ls.nvim")
+  use("jayp0521/mason-null-ls.nvim")
 
-	use({
-		"nvim-treesitter/nvim-treesitter",
-	})
-	use("akinsho/toggleterm.nvim")
+  use({
+    "nvim-treesitter/nvim-treesitter",
+  })
+  use("akinsho/toggleterm.nvim")
 
-	use("max397574/better-escape.nvim")
-	use("goolord/alpha-nvim")
-	use("ahmedkhalf/project.nvim")
+  use("max397574/better-escape.nvim")
+  use("goolord/alpha-nvim")
+  use("ahmedkhalf/project.nvim")
 
-	-- easy motion
-	use({
-		"phaazon/hop.nvim",
-		branch = "v2",
-	})
+  -- easy motion
+  use({
+    "phaazon/hop.nvim",
+    branch = "v2",
+  })
 
-	use({
-		"kylechui/nvim-surround",
-		tag = "*",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	})
+  use({
+    "kylechui/nvim-surround",
+    tag = "*",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  })
+  use("ethanholz/nvim-lastplace")
 
-	use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "kyazdani42/nvim-web-devicons" })
+  use({ "akinsho/bufferline.nvim", tag = "v3.*", requires = "kyazdani42/nvim-web-devicons" })
 
-	-- use({
-	--   "lewis6991/gitsigns.nvim",
-	--   -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
-	-- })
+  -- use({
+  --   "lewis6991/gitsigns.nvim",
+  --   -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+  -- })
 
-	use("windwp/nvim-autopairs")
-	use("windwp/nvim-ts-autotag")
-	use("numToStr/Comment.nvim")
+  use("windwp/nvim-autopairs")
+  use("windwp/nvim-ts-autotag")
+  use("numToStr/Comment.nvim")
 
-	-- key mapping
-	use("folke/which-key.nvim")
+  -- key mapping
+  use("folke/which-key.nvim")
 end)
